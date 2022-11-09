fb74d4087d845449c9b9f22fd199fef3403d0f9a
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-11-09 18:56:34 -0500

    5. Quick Introduction to Jasmine - Test Suites and Specifications

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,9 @@
+describe('CalculatorService', () => {
+    it('should add two numbers', () => {
+        pending();
+    });
+    
+    it('should subtract two numbers', () => {
+        fail();
+    });
+});
