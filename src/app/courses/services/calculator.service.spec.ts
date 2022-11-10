bb16fb87115e1f34f991e50f2592940b758ae699
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-11-09 21:35:25 -0500

    8. Structuring Angular Unit Tests - Test Setup using beforeEach

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,19 +1,26 @@
 import { CalculatorService } from "./calculator.service";
-import { LoggerService } from "./logger.service";
 
 describe('CalculatorService', () => {
+    let loggerSpy: any; 
+    let calculator: CalculatorService;
+
+    beforeEach(() => {
+        console.log("Calling beforeEach")
+        loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+        calculator = new CalculatorService(loggerSpy);
+    });
+
     it('should add two numbers', () => {
-        // const logger = new LoggerService();
-        const logger = jasmine.createSpyObj('LoggerService', ['log']);
-        const calculator = new CalculatorService(logger);
+        console.log("Add test");
         const result = calculator.add(2,2);
         expect(result).toBe(4);
-        expect(logger.log).toHaveBeenCalledTimes(1);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
     
     it('should subtract two numbers', () => {
-        const calculator = new CalculatorService(new LoggerService());
+        console.log("Subtract test");
         const result = calculator.subtract(2,2);
         expect(result).toBe(0, "unexpected subtraction result");
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
     });
 });
