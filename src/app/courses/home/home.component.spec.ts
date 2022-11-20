bc4bd6b931ab7fd761b6cf1b43b88f914d8c60b0
Victor Pinedo <victorpinedomartinez@gmail.com>
2022-11-20 18:35:09 -0500

    26. Introduction to Asynchronous Angular Testing - Understanding Jasmine done()

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -74,14 +74,18 @@ describe("HomeComponent", () => {
     expect(tabs.length).toBe(2, "Expected to find 2 tabs");
   });
 
-  it("should display advanced courses when tab clicked", () => {
+  it("should display advanced courses when tab clicked", (done: DoneFn) => {
     coursesService.findAllCourses.and.returnValue(of(setupCourses()));    
     fixture.detectChanges();
     const tabs = el.queryAll(By.css(".mat-tab-label"));
     el.nativeElement.click(tabs[1]);
     fixture.detectChanges();
-    const cardTitles = el.queryAll(By.css(".mat-card-title"));
-    expect(cardTitles.length).toBeGreaterThan(0, "Could not find card titles");
-    expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
+
+    setTimeout(() => {
+      const cardTitles = el.queryAll(By.css(".mat-card-title"));
+      expect(cardTitles.length).toBeGreaterThan(0, "Could not find card titles");
+      expect(cardTitles[0].nativeElement.textContent).toContain("Angular Security Course");
+      done();
+    }, 500);
   });
 });
